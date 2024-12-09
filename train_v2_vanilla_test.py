2890ddf2209d14b0181525319a179ad09bbc2033
WinstonChen2017 <weijiechen1994@gmail.com>
2024-12-09 17:05:36 -0600

    Original post

--- train_v2_vanilla_test.py
+++ train_v2_vanilla_test.py
@@ -20,7 +20,7 @@ data_division_file = "James_data_v3/cv_list.json"
 seeds = 729
 base_learning_rate = 1e-4
 num_frames = 5
-batch_size = 18
+batch_size = 8
 # sampling_timesteps = 50
 
 set_param("cv", 0)
