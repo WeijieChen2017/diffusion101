022adc4651c4b9bac8197e6870b18206b66589e5
WinstonChen2017 <weijiechen1994@gmail.com>
2025-01-07 17:46:44 -0600

    MAISI train_v1

--- maisi/train_v1.py
+++ maisi/train_v1.py
@@ -137,6 +137,7 @@ def main():
 
     # set the GPU index
     device = torch.device(f"cuda:{args.gpu}" if torch.cuda.is_available() else "cpu")
+    print(f"Device: {device}")
 
     autoencoder = download_and_reload_ckpt()
 
@@ -203,9 +204,9 @@ def main():
         train_loss = 0.0
         for i, batch in enumerate(data_loader_train):
             # in the data loader, the input is a tuple of (input, label, mask)
-            data_PET = batch["PET"].to(device).type(torch.float16)
-            data_CT = batch["CT"].to(device).type(torch.float16)
-            data_mask = batch["BODY"].to(device).type(torch.float16)
+            data_PET = batch["PET"].to(device).type(torch.float32)
+            data_CT = batch["CT"].to(device).type(torch.float32)
+            data_mask = batch["BODY"].to(device).type(torch.float32)
             # print the data shape of all three data
             print("data_PET shape: ", data_PET.shape, data_PET.dtype)
             print("data_CT shape: ", data_CT.shape, data_CT.dtype)
@@ -224,9 +225,9 @@ def main():
         val_loss = 0.0
         with torch.no_grad():
             for i, batch in enumerate(data_loader_val):
-                data_PET = batch["PET"].to(device).type(torch.float16)
-                data_CT = batch["CT"].to(device).type(torch.float16)
-                data_mask = batch["BODY"].to(device).type(torch.float16)
+                data_PET = batch["PET"].to(device).type(torch.float32)
+                data_CT = batch["CT"].to(device).type(torch.float32)
+                data_mask = batch["BODY"].to(device).type(torch.float32)
                 outputs = autoencoder(data_PET)
                 loss = loss_fn(outputs[0], data_CT)
                 loss = loss * data_mask
