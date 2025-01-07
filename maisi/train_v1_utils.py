5a3cb7998ac0d741eb2b4cdb8a6d038cb12f5f06
WinstonChen2017 <weijiechen1994@gmail.com>
2025-01-07 17:10:33 -0600

    MAISI train_v1

--- maisi/train_v1_utils.py
+++ maisi/train_v1_utils.py
@@ -165,4 +165,8 @@ def create_data_loader(
         
         return_dict["test_loader"] = test_loader
     
+    # print all return keys
+    for key in return_dict.keys():
+        print(key)
+
     return return_dict
