d2c5cee246d24688544dcd53100937f61e9860ea
WinstonChen2017 <weijiechen1994@gmail.com>
2025-01-07 18:07:41 -0600

    MAISI train_v1

--- maisi/train_v1.py
+++ maisi/train_v1.py
@@ -108,11 +108,11 @@ def main():
     # get the image dim y for each batch
     parser.add_argument("--dim_y", type=int, default=256, help="Image dimension y.")
     # get the image dim z for each batch
-    parser.add_argument("--dim_z", type=int, default=32, help="Image dimension z.")
+    parser.add_argument("--dim_z", type=int, default=64, help="Image dimension z.")
     # get the training epochs, default is 300
     parser.add_argument("--epochs", type=int, default=300, help="Number of training epochs.")
     # get the batchsize
-    parser.add_argument("--batchsize", type=int, default=4, help="Batch size.")
+    parser.add_argument("--batchsize", type=int, default=1, help="Batch size.")
     # get the loss function, default is "mae"
     parser.add_argument("--loss", type=str, default="MAE", help="Loss function.")
     # get the random GPU index, default is 4
