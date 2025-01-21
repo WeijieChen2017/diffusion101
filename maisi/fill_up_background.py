d736fccfd6646ec778e31572228b3d4addba5e35
WinstonChen2017 <weijiechen1994@gmail.com>
2025-01-21 02:01:53 -0600

    MAISI inference

--- maisi/fill_up_background.py
+++ maisi/fill_up_background.py
@@ -1,13 +1,14 @@
 seg_map_folder = "CTACIVV_MAISeg" # 512, 512, 335 1.367, 1.367, 3.27
+body_contour_folder = "../James_data_v3/mask"
 # find every .nii.gz file in this folder
 
 # load all files in the target folder
 import os
 import nibabel as nib
 
-file_list = sorted(os.listdir(seg_map_folder))
+file_list = sorted(os.listdir(body_contour_folder+"/mask_body_contour_*.nii.gz"))
 print(f"We find {len(file_list)} files in the target folder")
 for file in file_list:
     new_name = file.replace(".nii.gz", "_Spacing15.nii.gz")
-    command = f"3dresample -dxyz 1.5 1.5 1.5 -rmode NN -prefix {new_name} -input {seg_map_folder}/{file}"
+    command = f"3dresample -dxyz 1.5 1.5 1.5 -rmode NN -prefix {new_name} -input {body_contour_folder}/{file}"
     print(command)
