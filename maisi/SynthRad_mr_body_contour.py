0a27a6ac6b2ad05bc0d81c0298dbe6da3e24be3e
WinstonChen2017 <weijiechen1994@gmail.com>
2025-01-23 22:25:44 -0600

    MAISI inference

--- maisi/SynthRad_mr_body_contour.py
+++ maisi/SynthRad_mr_body_contour.py
@@ -2,9 +2,11 @@ import nibabel as nib
 import glob
 import os
 
+from skimage.morphology import binary_fill_holes
+
 work_dir = "."
 mr_list = sorted(glob.glob(f"{work_dir}/*.nii.gz"))
-mr_th = 250
+mr_th = 10
 for path in mr_list:
     nii_file = nib.load(path)
     nii_data = nii_file.get_fdata()
@@ -14,5 +16,11 @@ for path in mr_list:
     body_contour_mask = body_contour_mask.astype(int)
     body_contour_nii = nib.Nifti1Image(body_contour_mask, nii_file.affine, nii_file.header)
     body_contour_path = path.replace(".nii.gz", "_con.nii.gz")
+
+    # take it slice by slice and use binary_fill_holes to fill up the mask
+    len_z = body_contour_mask.shape[2]
+    for z in range(len_z):
+        body_contour_mask[:, :, z] = binary_fill_holes(body_contour_mask[:, :, z])
+
     nib.save(body_contour_nii, body_contour_path)
     print(f"Saved body contour to {body_contour_path}")
