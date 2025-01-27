419bab691af35759c908db8a646344755f3649a4
WinstonChen2017 <weijiechen1994@gmail.com>
2025-01-26 19:03:11 -0600

    MAISI inference

--- maisi/SynthRad_postprocessing.py
+++ maisi/SynthRad_postprocessing.py
@@ -69,31 +69,41 @@ for case_name in case_list:
     nonneg_synCT_bg[nonneg_synCT_bg > maxHU - minHU] = maxHU - minHU
 
     # MAE based on body contour con_data
-    case_mae = np.mean(np.abs(nonneg_synCT_bg[con_data > 0.5] - nonneg_ct[con_data > 0.5]))
-    case_mae_mid = np.mean(np.abs(nonneg_synCT_bg[con_data > 0.5][:, :, 1:-1] - nonneg_ct[con_data > 0.5][:, :, 1:-1]))
-    metric_mae += case_mae
-    metric_mae_mid += case_mae_mid
+    # case_mae = np.mean(np.abs(nonneg_synCT_bg[con_data > 0.5] - nonneg_ct[con_data > 0.5]))
+    # case_mae_mid = np.mean(np.abs(nonneg_synCT_bg[con_data > 0.5][:, :, 1:-1] - nonneg_ct[con_data > 0.5][:, :, 1:-1]))
+    # metric_mae += case_mae
+    # metric_mae_mid += case_mae_mid
 
     # SSIM and PSNRbased on body contour con_data [-1024, 3000]
     len_z = con_data.shape[2]
+    case_mae = 0
     case_ssim = 0
     case_psnr = 0
+    case_mae_mid = 0
     case_ssim_mid = 0
     case_psnr_mid = 0
     for z in range(len_z):
         masked_ct = nonneg_ct[:, :, z][con_data[:, :, z] > 0.5]
         masked_synCT_bg = nonneg_synCT_bg[:, :, z][con_data[:, :, z] > 0.5]
+        case_mae += np.mean(np.abs(masked_ct - masked_synCT_bg))
         case_ssim += ssim(masked_ct, masked_synCT_bg, data_range=maxHU - minHU)
         case_psnr += psnr(masked_ct, masked_synCT_bg, data_range=maxHU - minHU)
         if z > 0 and z < len_z - 1:
+            case_mae_mid += np.mean(np.abs(masked_ct - masked_synCT_bg))
             case_ssim_mid += ssim(masked_ct, masked_synCT_bg, data_range=maxHU - minHU)
             case_psnr_mid += psnr(masked_ct, masked_synCT_bg, data_range=maxHU - minHU)
+
+    case_mae /= len_z
     case_ssim /= len_z
     case_psnr /= len_z
+    case_mae_mid /= (len_z - 2)
     case_ssim_mid /= (len_z - 2)
     case_psnr_mid /= (len_z - 2)
+
+    metric_mae += case_mae
     metric_ssim += case_ssim
     metric_psnr += case_psnr
+    metric_mae_mid += case_mae_mid
     metric_ssim_mid += case_ssim_mid
     metric_psnr_mid += case_psnr_mid
 
@@ -102,9 +112,9 @@ for case_name in case_list:
 metric_mae /= len(case_list)
 metric_ssim /= len(case_list)
 metric_psnr /= len(case_list)
-metric_mae_mid /= len(case_list - 2)
-metric_ssim_mid /= len(case_list - 2)
-metric_psnr_mid /= len(case_list - 2)
+metric_mae_mid /= len(case_list)
+metric_ssim_mid /= len(case_list)
+metric_psnr_mid /= len(case_list)
 
 print(f"Overall: MAE {metric_mae:.4f}, SSIM {metric_ssim:.4f}, PSNR {metric_psnr:.4f}, MAE mid {metric_mae_mid:.4f}, SSIM mid {metric_ssim_mid:.4f}, PSNR mid {metric_psnr_mid:.4f}")
 
