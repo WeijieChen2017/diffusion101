89be19850d5ba622537e203fcf6d883f0cbd8158
WinstonChen2017 <weijiechen1994@gmail.com>
2025-01-30 16:38:08 -0600

    MAISI inference

--- maisi/James_NAC_CTAC_TS_LDM_inference.py
+++ maisi/James_NAC_CTAC_TS_LDM_inference.py
@@ -22,16 +22,20 @@ from monai.inferers import sliding_window_inference
 from skimage.metrics import structural_similarity as ssim
 from skimage.metrics import peak_signal_noise_ratio as psnr
 
+# case_name_list = [
+#     'E4055', 'E4058', 'E4061', 'E4066', 'E4068',
+#     'E4069', 'E4073', 'E4074', 'E4077', 'E4078',
+#     'E4079', 'E4081', 'E4084', 'E4091', 'E4092',
+#     'E4094', 'E4096', 'E4098', 'E4099', 'E4103',
+#     'E4105', 'E4106', 'E4114', 'E4115', 'E4118',
+#     'E4120', 'E4124', 'E4125', 'E4128', 'E4129',
+#     'E4130', 'E4131', 'E4134', 'E4137', 'E4138',
+#     'E4139', 
+# ]
 case_name_list = [
-    'E4055', 'E4058', 'E4061', 'E4066', 'E4068',
-    'E4069', 'E4073', 'E4074', 'E4077', 'E4078',
-    'E4079', 'E4081', 'E4084', 'E4091', 'E4092',
-    'E4094', 'E4096', 'E4098', 'E4099', 'E4103',
-    'E4105', 'E4106', 'E4114', 'E4115', 'E4118',
-    'E4120', 'E4124', 'E4125', 'E4128', 'E4129',
-    'E4130', 'E4131', 'E4134', 'E4137', 'E4138',
-    'E4139', 
+    'E4124', 'E4128', 'E4130', 'E4134', 'E4138',
 ]
+
 # E4063, E4080, E4087, E4097, E4102, E4289 are removed for z mismatch
 
 # here ask for the input of case_list division
