d327f290c8f60e9678a2047a3c7bfea778a96765
WinstonChen2017 <weijiechen1994@gmail.com>
2025-02-18 17:38:56 -0600

    load CTAC_maisi

--- maisi/Recover_CTAC_maisi.py
+++ maisi/Recover_CTAC_maisi.py
@@ -46,3 +46,40 @@ for case_name in case_name_list:
     
     # output commands
     print(command1)
+
+# 3dresample -dxyz 1.3672 1.3672 3.2700 -prefix James_36/CTAC_maisi/CTAC_maisi_E4055_v1_3dresample.nii.gz -input NAC_CTAC_Spacing15/inference_20250128_noon/CTAC_E4055_TS_MAISI.nii.gz
+# 3dresample -dxyz 1.3672 1.3672 3.2700 -prefix James_36/CTAC_maisi/CTAC_maisi_E4058_v1_3dresample.nii.gz -input NAC_CTAC_Spacing15/inference_20250128_noon/CTAC_E4058_TS_MAISI.nii.gz
+# 3dresample -dxyz 1.3672 1.3672 3.2700 -prefix James_36/CTAC_maisi/CTAC_maisi_E4061_v1_3dresample.nii.gz -input NAC_CTAC_Spacing15/inference_20250128_noon/CTAC_E4061_TS_MAISI.nii.gz
+# 3dresample -dxyz 1.3672 1.3672 3.2700 -prefix James_36/CTAC_maisi/CTAC_maisi_E4066_v1_3dresample.nii.gz -input NAC_CTAC_Spacing15/inference_20250128_noon/CTAC_E4066_TS_MAISI.nii.gz
+# 3dresample -dxyz 1.3672 1.3672 3.2700 -prefix James_36/CTAC_maisi/CTAC_maisi_E4068_v1_3dresample.nii.gz -input NAC_CTAC_Spacing15/inference_20250128_noon/CTAC_E4068_TS_MAISI.nii.gz
+# 3dresample -dxyz 1.3672 1.3672 3.2700 -prefix James_36/CTAC_maisi/CTAC_maisi_E4069_v1_3dresample.nii.gz -input NAC_CTAC_Spacing15/inference_20250128_noon/CTAC_E4069_TS_MAISI.nii.gz
+# 3dresample -dxyz 1.3672 1.3672 3.2700 -prefix James_36/CTAC_maisi/CTAC_maisi_E4073_v1_3dresample.nii.gz -input NAC_CTAC_Spacing15/inference_20250128_noon/CTAC_E4073_TS_MAISI.nii.gz
+# 3dresample -dxyz 1.3672 1.3672 3.2700 -prefix James_36/CTAC_maisi/CTAC_maisi_E4074_v1_3dresample.nii.gz -input NAC_CTAC_Spacing15/inference_20250128_noon/CTAC_E4074_TS_MAISI.nii.gz
+# 3dresample -dxyz 1.3672 1.3672 3.2700 -prefix James_36/CTAC_maisi/CTAC_maisi_E4077_v1_3dresample.nii.gz -input NAC_CTAC_Spacing15/inference_20250128_noon/CTAC_E4077_TS_MAISI.nii.gz
+# 3dresample -dxyz 1.3672 1.3672 3.2700 -prefix James_36/CTAC_maisi/CTAC_maisi_E4078_v1_3dresample.nii.gz -input NAC_CTAC_Spacing15/inference_20250128_noon/CTAC_E4078_TS_MAISI.nii.gz
+# 3dresample -dxyz 1.3672 1.3672 3.2700 -prefix James_36/CTAC_maisi/CTAC_maisi_E4079_v1_3dresample.nii.gz -input NAC_CTAC_Spacing15/inference_20250128_noon/CTAC_E4079_TS_MAISI.nii.gz
+# 3dresample -dxyz 1.3672 1.3672 3.2700 -prefix James_36/CTAC_maisi/CTAC_maisi_E4081_v1_3dresample.nii.gz -input NAC_CTAC_Spacing15/inference_20250128_noon/CTAC_E4081_TS_MAISI.nii.gz
+# 3dresample -dxyz 1.3672 1.3672 3.2700 -prefix James_36/CTAC_maisi/CTAC_maisi_E4084_v1_3dresample.nii.gz -input NAC_CTAC_Spacing15/inference_20250128_noon/CTAC_E4084_TS_MAISI.nii.gz
+# 3dresample -dxyz 1.3672 1.3672 3.2700 -prefix James_36/CTAC_maisi/CTAC_maisi_E4091_v1_3dresample.nii.gz -input NAC_CTAC_Spacing15/inference_20250128_noon/CTAC_E4091_TS_MAISI.nii.gz
+# 3dresample -dxyz 1.3672 1.3672 3.2700 -prefix James_36/CTAC_maisi/CTAC_maisi_E4092_v1_3dresample.nii.gz -input NAC_CTAC_Spacing15/inference_20250128_noon/CTAC_E4092_TS_MAISI.nii.gz
+# 3dresample -dxyz 1.3672 1.3672 3.2700 -prefix James_36/CTAC_maisi/CTAC_maisi_E4094_v1_3dresample.nii.gz -input NAC_CTAC_Spacing15/inference_20250128_noon/CTAC_E4094_TS_MAISI.nii.gz
+# 3dresample -dxyz 1.3672 1.3672 3.2700 -prefix James_36/CTAC_maisi/CTAC_maisi_E4096_v1_3dresample.nii.gz -input NAC_CTAC_Spacing15/inference_20250128_noon/CTAC_E4096_TS_MAISI.nii.gz
+# 3dresample -dxyz 1.3672 1.3672 3.2700 -prefix James_36/CTAC_maisi/CTAC_maisi_E4098_v1_3dresample.nii.gz -input NAC_CTAC_Spacing15/inference_20250128_noon/CTAC_E4098_TS_MAISI.nii.gz
+# 3dresample -dxyz 1.3672 1.3672 3.2700 -prefix James_36/CTAC_maisi/CTAC_maisi_E4099_v1_3dresample.nii.gz -input NAC_CTAC_Spacing15/inference_20250128_noon/CTAC_E4099_TS_MAISI.nii.gz
+# 3dresample -dxyz 1.3672 1.3672 3.2700 -prefix James_36/CTAC_maisi/CTAC_maisi_E4103_v1_3dresample.nii.gz -input NAC_CTAC_Spacing15/inference_20250128_noon/CTAC_E4103_TS_MAISI.nii.gz
+# 3dresample -dxyz 1.3672 1.3672 3.2700 -prefix James_36/CTAC_maisi/CTAC_maisi_E4105_v1_3dresample.nii.gz -input NAC_CTAC_Spacing15/inference_20250128_noon/CTAC_E4105_TS_MAISI.nii.gz
+# 3dresample -dxyz 1.3672 1.3672 3.2700 -prefix James_36/CTAC_maisi/CTAC_maisi_E4106_v1_3dresample.nii.gz -input NAC_CTAC_Spacing15/inference_20250128_noon/CTAC_E4106_TS_MAISI.nii.gz
+# 3dresample -dxyz 1.3672 1.3672 3.2700 -prefix James_36/CTAC_maisi/CTAC_maisi_E4114_v1_3dresample.nii.gz -input NAC_CTAC_Spacing15/inference_20250128_noon/CTAC_E4114_TS_MAISI.nii.gz
+# 3dresample -dxyz 1.3672 1.3672 3.2700 -prefix James_36/CTAC_maisi/CTAC_maisi_E4115_v1_3dresample.nii.gz -input NAC_CTAC_Spacing15/inference_20250128_noon/CTAC_E4115_TS_MAISI.nii.gz
+# 3dresample -dxyz 1.3672 1.3672 3.2700 -prefix James_36/CTAC_maisi/CTAC_maisi_E4118_v1_3dresample.nii.gz -input NAC_CTAC_Spacing15/inference_20250128_noon/CTAC_E4118_TS_MAISI.nii.gz
+# 3dresample -dxyz 1.3672 1.3672 3.2700 -prefix James_36/CTAC_maisi/CTAC_maisi_E4120_v1_3dresample.nii.gz -input NAC_CTAC_Spacing15/inference_20250128_noon/CTAC_E4120_TS_MAISI.nii.gz
+# 3dresample -dxyz 1.3672 1.3672 3.2700 -prefix James_36/CTAC_maisi/CTAC_maisi_E4124_v1_3dresample.nii.gz -input NAC_CTAC_Spacing15/inference_20250128_noon/CTAC_E4124_TS_MAISI.nii.gz
+# 3dresample -dxyz 1.3672 1.3672 3.2700 -prefix James_36/CTAC_maisi/CTAC_maisi_E4125_v1_3dresample.nii.gz -input NAC_CTAC_Spacing15/inference_20250128_noon/CTAC_E4125_TS_MAISI.nii.gz
+# 3dresample -dxyz 1.3672 1.3672 3.2700 -prefix James_36/CTAC_maisi/CTAC_maisi_E4128_v1_3dresample.nii.gz -input NAC_CTAC_Spacing15/inference_20250128_noon/CTAC_E4128_TS_MAISI.nii.gz
+# 3dresample -dxyz 1.3672 1.3672 3.2700 -prefix James_36/CTAC_maisi/CTAC_maisi_E4129_v1_3dresample.nii.gz -input NAC_CTAC_Spacing15/inference_20250128_noon/CTAC_E4129_TS_MAISI.nii.gz
+# 3dresample -dxyz 1.3672 1.3672 3.2700 -prefix James_36/CTAC_maisi/CTAC_maisi_E4130_v1_3dresample.nii.gz -input NAC_CTAC_Spacing15/inference_20250128_noon/CTAC_E4130_TS_MAISI.nii.gz
+# 3dresample -dxyz 1.3672 1.3672 3.2700 -prefix James_36/CTAC_maisi/CTAC_maisi_E4131_v1_3dresample.nii.gz -input NAC_CTAC_Spacing15/inference_20250128_noon/CTAC_E4131_TS_MAISI.nii.gz
+# 3dresample -dxyz 1.3672 1.3672 3.2700 -prefix James_36/CTAC_maisi/CTAC_maisi_E4134_v1_3dresample.nii.gz -input NAC_CTAC_Spacing15/inference_20250128_noon/CTAC_E4134_TS_MAISI.nii.gz
+# 3dresample -dxyz 1.3672 1.3672 3.2700 -prefix James_36/CTAC_maisi/CTAC_maisi_E4137_v1_3dresample.nii.gz -input NAC_CTAC_Spacing15/inference_20250128_noon/CTAC_E4137_TS_MAISI.nii.gz
+# 3dresample -dxyz 1.3672 1.3672 3.2700 -prefix James_36/CTAC_maisi/CTAC_maisi_E4138_v1_3dresample.nii.gz -input NAC_CTAC_Spacing15/inference_20250128_noon/CTAC_E4138_TS_MAISI.nii.gz
+# 3dresample -dxyz 1.3672 1.3672 3.2700 -prefix James_36/CTAC_maisi/CTAC_maisi_E4139_v1_3dresample.nii.gz -input NAC_CTAC_Spacing15/inference_20250128_noon/CTAC_E4139_TS_MAISI.nii.gz
